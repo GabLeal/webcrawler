f08a894eefdc73c036b107ea1a872663ee0cc49c
Gabriel Leal dos Santos <gabriel.leal@CTNDELL76HCJH3.pampa.compasso>
2021-11-03 00:45:07 -0300

    first commit

---
+++ rabbitmq-server.js
@@ -0,0 +1,31 @@
+const amqplib = require('amqplib');
+
+
+class RabbitMqService{
+    // 15672
+
+    constructor(){
+       
+      this.conn
+      this.channel
+    }
+
+    async start(){
+        console.log("entrou no start")
+       this.conn = await amqplib.connect({
+        host: 'localhost',
+        port: 5672,
+        username: 'guest',
+        password: 'guest'
+      })
+       this.channel = await this.conn.createChannel();
+    }
+
+    async publishInQueue(queue, message){
+        console.log("publicando na fila")
+        return this.channel.sendToQueue(queue, Buffer.from(message))
+    }
+
+}
+
+module.exports = RabbitMqService
